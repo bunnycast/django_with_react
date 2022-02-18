fece1b3243985a7569d6005c7cfc0687c41da971
bunnycast <berzzubunny@gmail.com>
2022-02-19 01:25:27 +0900

    1-2. Django Tutorial - admin  - admin class with decorator (@admin.register(ModelName))  - list_display  - list_display_links  - list_filter  - search_fields  - __str__  - custom function(message length)

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from instagram.models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'message_length', 'is_public', 'created_at', 'updated_at']
+    list_display_links = ['message']
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['message']
+
+    # # model에서 함수 쓰는 것과 똑같은 효과
+    # def message_length(self, post):
+    #     return f"{len(post.message)} 글자"
+    #
+    # message_length.short_description = "메시지 글자 수"
--- instagram/models.py
+++ instagram/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False, verbose_name="공개여부")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.message
+
+    def message_length(self):
+        return f"{len(self.message)} 글자"
+
+    message_length.short_description = "메시지 글자 수"
