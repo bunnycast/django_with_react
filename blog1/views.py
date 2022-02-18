b95b64f599dcecf1b72c4bc716f06865ff467703
bunnycast <berzzubunny@gmail.com>
2022-02-19 00:58:33 +0900

    1. Django Tutorial - blog1, instagram

---
+++ blog1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from blog1.models import Post
+
+
+# Create your views here.
+
+def post_list(request):
+    qs = Post.objects.all()
+    return render(request, 'blog1/post_list.html', {
+        'post_list': qs,
+    })
