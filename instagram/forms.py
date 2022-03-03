8e1e285afaa205acedff5cd1e2d58cc88fe9909b
bunnycast <berzzubunny@gmail.com>
2022-03-03 22:29:32 +0900

    25. form validation

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from .models import Post
 
@@ -7,3 +9,18 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['message', 'photo', 'tag_set', 'is_public']
         # exclude = []
+
+    def clean_message(self):
+        message = self.cleaned_data.get('message')
+        if message:
+            message = re.sub(r'[a-zA-Z]+', '', message)
+        return message
+
+    # validators는 가급적 모델에 정의, modelform을 통해 모델의 validators 정보도 같이 가져와야 함
+
+    """
+    clean이 필요한 경우
+    특정 form에서 1회성 유효성 검사 루틴이 필요할 때
+    다수 필드값에 걸쳐 유효성 검사가 필요할 때
+    필드 값을 변경해야 할 필요가 있을 때(validator는 값을 체크만 하고, 변경은 안됨   
+    """
