1dd8683414ecc929d9465c84006666e09cc7d908
bunnycast <berzzubunny@gmail.com>
2022-03-05 10:39:00 +0900

    30. accounts profile update

--- accounts/models.py
+++ accounts/models.py
@@ -5,5 +5,5 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    address = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=6)  # validators=[]
+    address = models.CharField(max_length=100, blank=True)
+    zipcode = models.CharField(max_length=6, blank=True)  # validators=[]
